0e62f69b912ef10013e83f54a08332dcb98d0107
khal_huntsman <hunter.jude.steele@gmail.com>
2025-12-08 18:10:34 -0600

    Made necessary changes to song.py per lab instructions to pass song_test.py

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,57 @@
+#!/usr/bin/env python3
+
+# Author: Hunter Steele
+# Date: 12/8/25
+# Version 1.1
+
 class Song:
-    pass
+    # class-level trackers shared by all Song instances
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # store instance data
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # update class-level bookkeeping for each new Song
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artist(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artist_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        # increment total number of Song instances
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        # track all genres used (duplicates allowed unless you choose otherwise)
+        cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artist(cls, artist):
+        # track all artists used
+        cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # count how many songs belong to each genre
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        # count how many songs each artist has
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
